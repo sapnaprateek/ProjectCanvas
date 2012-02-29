f2fa4cb881c41388ee81373798bb8f13b8e356bc
Prateek Jain <prateek@sapnasolutions.com>
2012-02-29 18:32:43 +0530

    New Node

--- js/script.js
+++ js/script.js
@@ -3,13 +3,22 @@ window.onload = function(){
 	var context = canvas.getContext("2d");
 	context.lineWidth = 3; 
 	context.strokeStyle = "#999999";
-	//context.arc(canvas.width / 2, canvas.height / 2 + 40, 100, 0 * Math.PI, 1/12 * Math.PI, false);
-	//context.stroke();
 	for( var i=0; i<54; i=i+2)
 	{
 	context.beginPath();
-	context.arc(canvas.width / 2, canvas.height / 2 + 40, 120, i/27 * Math.PI, (i+1)/27 * Math.PI, false);
+	context.arc(canvas.width / 2, canvas.height / 2, 100, i/27 * Math.PI, (i+1)/27 * Math.PI, false);
 	context.stroke();
 	}
+	for(var i = 0; i<=2; i = i+0.2){
+	context.beginPath();
+	context.moveTo((canvas.width / 2) + 80 + (i*80), (canvas.height / 2) - 60 - i*60);
+	context.lineTo((canvas.width / 2) + 80 + ((i+0.1)*80), (canvas.height / 2 ) - 60 - ((i+0.1)*60));
+	context.stroke();
+	}
+	
+	context.beginPath();
+	context.strokeStyle = "#00685b";
+	context.arc(canvas.width / 2 + 264, canvas.height/2 - 198 , 20 , 0 , 2*Math.PI, false);
+	context.stroke();
 	
 }
